27a0e3b78865977f3237964201221069776618ef
Tony Brix <tony@brix.ninja>
2020-07-29 01:35:41 -0500

    fix: use etch

--- spec/notifications-count-spec.js
+++ spec/notifications-count-spec.js
@@ -10,18 +10,18 @@ describe("Notifications Count", () => {
 
 		await Promise.all([
 			atom.packages.activatePackage("notifications-plus"),
-			atom.packages.activatePackage("status-bar")
+			atom.packages.activatePackage("status-bar"),
 		]);
 
 		({
-			statusBarManager
+			statusBarManager,
 		} = atom.packages.getActivePackage("notifications-plus").mainModule);
 		notificationsCountContainer = workspaceElement.querySelector(".notifications-count");
 	});
 
 	describe("when the package is activated", () => {
 		it("attaches an .notifications-count element to the dom", () => {
-			expect(statusBarManager.count).toBe(0);
+			expect(statusBarManager.state.count).toBe(0);
 			expect(notificationsCountContainer).toExist();
 		});
 	});
@@ -42,10 +42,10 @@ describe("Notifications Count", () => {
 			await atom.packages.activatePackage("notifications-plus");
 
 			({
-				statusBarManager
+				statusBarManager,
 			} = atom.packages.getActivePackage("notifications-plus").mainModule);
 			notificationsCountContainer = workspaceElement.querySelector(".notifications-count");
-			expect(statusBarManager.count).toBe(2);
+			expect(statusBarManager.state.count).toBe(2);
 			expect(parseInt(notificationsCountContainer.textContent, 10)).toBe(2);
 		});
 	});
@@ -55,31 +55,38 @@ describe("Notifications Count", () => {
 			generateFakeFetchResponses();
 		});
 
-		it("will add the new-notification class for as long as the animation", () => {
+		it("will add the new-notification class for as long as the animation", async () => {
 			const notificationsCountNumber = notificationsCountContainer.firstChild.firstChild;
 			expect(notificationsCountNumber).not.toHaveClass("new-notification");
 			atom.notifications.addInfo("A message");
+			await statusBarManager.update();
 			expect(notificationsCountNumber).toHaveClass("new-notification");
 
 			const animationend = new AnimationEvent("animationend", {animationName: "new-notification"});
 			notificationsCountNumber.dispatchEvent(animationend);
+			await statusBarManager.update();
 			expect(notificationsCountNumber).not.toHaveClass("new-notification");
 		});
 
-		it("changes the .notifications-count element last-type attribute corresponding to the type", () => {
+		it("changes the .notifications-count element last-type attribute corresponding to the type", async () => {
 			atom.notifications.addSuccess("A message");
+			await statusBarManager.update();
 			expect(notificationsCountContainer.getAttribute("last-type")).toBe("success");
 
 			atom.notifications.addInfo("A message");
+			await statusBarManager.update();
 			expect(notificationsCountContainer.getAttribute("last-type")).toBe("info");
 
 			atom.notifications.addWarning("A message");
+			await statusBarManager.update();
 			expect(notificationsCountContainer.getAttribute("last-type")).toBe("warning");
 
 			atom.notifications.addError("A message");
+			await statusBarManager.update();
 			expect(notificationsCountContainer.getAttribute("last-type")).toBe("error");
 
 			atom.notifications.addFatalError("A message");
+			await statusBarManager.update();
 			expect(notificationsCountContainer.getAttribute("last-type")).toBe("fatal");
 		});
 	});
